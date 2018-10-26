365ca6abb5073896f89f6d2a89c9b3722baa3a8b
Trong Tran <trong.tn178@gmail.com>
2018-10-26 10:46:22 -0700

    Initial project

---
+++ src/com/noithatchatluong/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.noithatchatluong.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/home")
+public class HomeController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Forward toi trang /WEB-INF/views/homeView.jsp
+		// (Người dùng không bao giờ truy cập trực tiếp được vào các trang JSP
+		// đặt trong WEB-INF)
+		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/templates/index.jsp");
+
+		dispatcher.forward(request, response);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
