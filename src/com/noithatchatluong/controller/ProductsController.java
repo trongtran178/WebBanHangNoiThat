af960d5576914b59224590b114ab8013eaa8054b
Trong Tran <trong.tn178@gmail.com>
2018-11-08 01:44:56 +0700

    Edit name - ProductsController

--- src/com/noithatchatluong/controller/ProductsController.java
+++ src/com/noithatchatluong/controller/ProductsController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/products")
-public class ProductController extends HttpServlet {
+public class ProductsController extends HttpServlet {
 	/**
 	 * 
 	 */
