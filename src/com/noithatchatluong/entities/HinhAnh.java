d99bb09fdf5685d84ed4c077f777341d18ad3d00
Trong Tran <trong.tn178@gmail.com>
2018-12-08 22:13:00 +0700

    edit

--- src/com/noithatchatluong/entities/HinhAnh.java
+++ src/com/noithatchatluong/entities/HinhAnh.java
@@ -1,5 +1,73 @@
 package com.noithatchatluong.entities;
 
-public class HinhAnh {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "HinhAnh")
+public class HinhAnh implements Serializable {
+
+	/**
+	 * 
+	 */
+	private Integer id;
+	private String maHinh;
+	private String hinhCha;
+	private String hinhCon;
+
+	@Id
+	@Column(name = "ID")
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Column(name = "MaHinh", nullable = false)
+	public String getMaHinh() {
+		return maHinh;
+	}
+
+	public void setMaHinh(String maHinh) {
+		this.maHinh = maHinh;
+	}
+
+	@Column(name = "HinhCha", nullable = false)
+	public String getHinhCha() {
+		return hinhCha;
+	}
+
+	public void setHinhCha(String hinhCha) {
+		this.hinhCha = hinhCha;
+	}
+
+	@Column(name = "HinhCon", nullable = false)
+	public String getHinhCon() {
+		return hinhCon;
+	}
+
+	public void setHinhCon(String hinhCon) {
+		this.hinhCon = hinhCon;
+	}
+
+/*	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "maHinh")
+	public SanPham getSanPham() {
+		return sanPham;
+	}
+
+	public void setSanPham(SanPham sanPham) {
+		this.sanPham = sanPham;
+	}*/
 
 }
